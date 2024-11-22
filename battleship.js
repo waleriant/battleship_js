c9cc6a7e5db9c8bf7dd641e3cca850e0d4c17dbe
waleriant <tranthien.valery@gmail.com>
2024-11-22 23:09:27 +0100

    second commit

--- battleship.js
+++ battleship.js
@@ -1,16 +1,20 @@
+/* buildBattleship(1, 3, 1, 6)
+   => [[1, 3, true], [1, 4, true], [1, 5, true], [1, 6, true]]
+   where "true" means that this piece of the battleship is intact. */
+
 function buildBattleship(x1, y1, x2, y2) {
-	myArray = [[x1, y1]]
+	structure = [[x1, y1, true]];
 	if (x1 != x2) {
 		if (x1 < x2) {
 			i = x1 + 1;
 			do {
-				myArray.push([i, y1]);
+				structure.push([i, y1, true]);
 				i++;
 			} while (i <= x2);
 		} else {
 			i = x1 - 1;
 			do {
-				myArray.push([i, y1]);
+				structure.push([i, y1, true]);
 				i--;
 			} while (i >= x2);		
 		}
@@ -18,22 +22,117 @@ function buildBattleship(x1, y1, x2, y2) {
 		if (y1 < y2) {
 			i = y1 + 1;
 			do {
-				myArray.push([x1, i]);
+				structure.push([x1, i, true]);
 				i++;
 			} while (i <= y2);
 		} else {
 			i = y1 - 1;
 			do {
-				myArray.push([x1, i]);
+				structure.push([x1, i, true]);
 				i--;
 			} while (i >= y2);		
 		}		
 	}
-	return myArray;
+	return structure;
 }
 
 class Battleship {
-	constructor(length, x1, y1, x2, y2) {
+	constructor(id, length, color, x1, y1, x2, y2) {
+		this.id = id;
+		this.length = length;
+		this.color = color;
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y1;
+		this.structure = buildBattleship(x1, y1, x2, y2);
+		this.fixed = false;
+		this.sunk = false;
+		
+		moveLeft() {
+			if (this.x1 - 1 < 1 || this.x2 - 1 < 1) {
+				return false;
+			} else {
+				let i = 0;
+				while (i < this.structure.length) {
+					this.structure[i][0]--;
+					i++;
+				}
+			}
+		}
+		
+		moveRight() {
+			if (this.x1 + 1 > 10 || this.x2 + 1 < 10) {
+				return false;
+			} else {
+				let i = 0;
+				while (i < this.structure.length) {
+					this.structure[i][0]++;
+					i++;
+				}
+			}
+		}
+		
+		moveDown() {
+			if (this.y1 - 1 < 1 || this.y2 - 1 < 1) {
+				return false;
+			} else {
+				let i = 0;
+				while (i < this.structure.length) {
+					this.structure[i][1]--;
+					i++;
+				}
+			}
+		}	
+		
+		moveUp() {
+			if (this.y1 + 1 > 10 || this.y2 + 1 > 10) {
+				return false;
+			} else {
+				let i = 0;
+				while (i < this.structure.length) {
+					this.structure[i][1]++;
+					i++;
+				}
+			}
+		}
+		
+		rotate() {
+			center = Math.ceil(this.structure.length/2);
+			if (this.x1 == this.x2) {
+
+			} else {
+				
+			}		
+		}
+		
+		// fixes the battleship => its position can no longer be modified
+		
+		fix() {
+			this.fixed = true;
+		}
+		
+		// sets the battleship as sunk
+		
+		setSunk() {
+			this.sunk = true;
+		}
+		
+		// if sunk == false, isSunk() checks each piece of the battleship's structure if needed
+		// if each piece is damaged (i.e. piece[2] == false), the battleship is set as sunk and isSunk() returns "true"
+		
+		isSunk() {
+			if (this.sunk == true) {
+				return true;
+			} else {
+				for (const piece of this.structure) {
+					if (piece[2] == true) {
+						return false;
+				}
+			}
+			this.setSunk();
+			return true;
+		}
 	}
 }
 
